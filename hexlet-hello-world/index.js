ac893e2157b0132b87c4a8e8d7a322713a4cf698
CriSmile92 <crismile206@gmail.com>
2025-11-11 17:42:47 +0300

    add prettier

--- hexlet-hello-world/index.js
+++ hexlet-hello-world/index.js
@@ -1,6 +1,6 @@
-import _ from 'lodash'
+import _ from "lodash";
+import _ from "blog";
 
+console.log("Hello, Hexlet!");
 
-console.log('Hello, Hexlet!');
-
-console.log(_.last(['one', 'two']));
+console.log(_.last(["one", "two"]));
